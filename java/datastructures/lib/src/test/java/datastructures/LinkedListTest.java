72172fbc18833eed0056e2daadff72b67967e4ff
Aaron Clark <amclarkwustl@gmail.com>
2023-07-23 11:24:36 -0700

    added skeleton for stack and queue

--- java/datastructures/lib/src/test/java/datastructures/LinkedListTest.java
+++ java/datastructures/lib/src/test/java/datastructures/LinkedListTest.java
@@ -1,5 +1,7 @@
-package datastructures.linkedlist;
+package datastructures;
 
+import datastructures.linkedlist.LinkedList;
+import datastructures.linkedlist.Node;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
