321bf6a501c791c7b4774637be99430d50964038
Aaron Clark <amclarkwustl@gmail.com>
2023-07-30 19:24:59 -0700

    initial setup of classes

---
+++ java/datastructures/lib/src/main/java/datastructures/tree/BinaryTree.java
@@ -0,0 +1,20 @@
+package datastructures.tree;
+
+import java.util.List;
+
+public class BinaryTree {
+
+  Node root;
+
+  public List<Integer> preOrder() {
+    return null;
+  }
+
+  public List<Integer> inOrder() {
+    return null;
+  }
+
+  public List<Integer> postOrder() {
+    return null
+  }
+}
