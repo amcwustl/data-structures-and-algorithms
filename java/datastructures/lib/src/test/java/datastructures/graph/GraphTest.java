44c94047caf5a737a505a49b257fe9b6c108b85d
Aaron Clark <amclarkwustl@gmail.com>
2023-08-26 18:14:55 -0700

    added add vertex test

--- java/datastructures/lib/src/test/java/datastructures/graph/GraphTest.java
+++ java/datastructures/lib/src/test/java/datastructures/graph/GraphTest.java
@@ -1,4 +1,21 @@
 package datastructures.graph;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class GraphTest {
+
+  @Test
+  public void testAddVertex() {
+    Graph<Integer> sut = new Graph<>(10); 
+
+    Vertex<Integer> vertex = sut.addVertex(42);
+
+    assertNotNull(vertex);
+    assertEquals(42, vertex.getValue());
+
+    String graphString = sut.toString();
+    assertTrue(graphString.contains("42"));
+  }
 }
