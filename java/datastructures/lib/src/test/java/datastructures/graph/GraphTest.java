c472b1f6d2119663049120e926409fd1531bf921
Aaron Clark <amclarkwustl@gmail.com>
2023-08-26 18:28:22 -0700

    added addEdge test to junit tests

--- java/datastructures/lib/src/test/java/datastructures/graph/GraphTest.java
+++ java/datastructures/lib/src/test/java/datastructures/graph/GraphTest.java
@@ -2,6 +2,8 @@ package datastructures.graph;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.LinkedList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class GraphTest {
@@ -17,5 +19,23 @@ public class GraphTest {
 
     String graphString = sut.toString();
     assertTrue(graphString.contains("42"));
+  }
+
+  @Test
+  public void testAddEdge() {
+    Graph<Integer> graph = new Graph<>(10);
+
+    Vertex<Integer> vertex1 = graph.addVertex(1);
+    Vertex<Integer> vertex2 = graph.addVertex(2);
+
+    graph.addEdge(vertex1, vertex2, 5);
+
+    LinkedList<Edge<Integer>> adjacencyList1 = graph.adjacencyLists.get(vertex1);
+    assertEquals(1, adjacencyList1.size());
+
+    Edge<Integer> edge = adjacencyList1.get(0);
+    assertEquals(vertex2, edge.getDestination());
+    assertEquals(5, edge.getWeight());
+    
   }
 }
